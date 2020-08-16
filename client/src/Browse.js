2c9ecd62e48bb6ade08cdfbcb276cdfaf133bf10
Melissa Gu <melissa.gu@richmond.edu>
2020-08-16 18:14:20 -0400

    browse murals page

---
+++ client/src/Browse.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+import viewerSubmission from './images/viewersubmission.jpg';
+
+class Submit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            murals: null
+        };
+    }
+    componentDidMount() {
+        this.callBackendAPI()
+            .then(res => {
+                this.setState({
+                    murals: res.murals
+                });
+            });
+    }
+    callBackendAPI = async () => {
+        const response = await fetch('/api/list');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+        return body;
+    }
+    render() {
+        if (this.state.murals) {
+            return (
+                <div className="pageContainer">
+                    <h3 style={{"margin-bottom": "30px", "margin-left": "15px"}}>Browse Murals</h3>
+                    <Container>
+                        <Row>
+                            {this.state.murals.map((mural, i) => {
+                                console.log(mural);
+                                return (
+                                    <Col key={i} xs={12} md={6} lg={4} style={{"margin-bottom": "30px"}}>
+                                        <Card className="h-100">
+                                            <a href={"/mural/" + mural.properties.id} as={Link} to={"/mural/" + mural.properties.id}>
+                                                <Card.Img 
+                                                    style={{
+                                                        "height": "200px", 
+                                                        "overflow": "hidden", 
+                                                        "object-fit": "cover"}} 
+                                                    variant="top" 
+                                                    src={mural.properties.images[0]} />
+                                                <Card.Body>
+                                                    <Card.Title
+                                                        style={{"color": "black"}}>
+                                                        {mural.properties.title}
+                                                    </Card.Title>
+                                                </Card.Body>
+                                            </a>
+                                        </Card>
+                                    </Col>
+                                );
+                            })}
+                        </Row>
+                    </Container>
+                </div>
+            ); 
+        }
+        else {
+            return (<div />);
+        }
+        
+    }
+}
+
+export default Submit;
--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -3,6 +3,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { Switch, Route, Link } from 'react-router-dom';
 import Mapbox from './Mapbox.js';
 import About from './About.js';
+import Browse from './Browse.js';
 import Submit from './Submit.js';
 import SubmitArtistForm from './SubmitArtistForm.js';
 import SubmitViewerForm from './SubmitViewerForm.js';
@@ -20,6 +21,7 @@ class Navigation extends Component {
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/browse">Browse Murals</Nav.Link>
                         <Nav.Link as={Link} to="/submit">Submit a Mural</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
@@ -27,6 +29,7 @@ class Navigation extends Component {
                 <Switch>
                     <Route exact path="/" component={Mapbox}/>
                     <Route exact path="/about" component={About}/>
+                    <Route exact path="/browse" component={Browse}/>
                     <Route exact path="/submit" component={Submit}/>
                     <Route exact path="/submitartist" component={SubmitArtistForm}/>
                     <Route exact path="/submitviewer" component={SubmitViewerForm}/>
