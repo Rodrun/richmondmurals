c8710217bf0001131ff9f5af13d50622b3c7e907
Melissa Gu <melissa.gu@richmond.edu>
2020-08-22 19:02:41 -0400

    viewer and artist upload to cloudinary

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const multer = require('multer');
 const upload = multer();
 const cloudinary = require('cloudinary').v2;
-const DatauriParser = require("datauri/parser");
-const parser = new DatauriParser();
+const DataUriParser = require("datauri/parser");
+const parser = new DataUriParser();
 // const fs = require('fs');
 
 const port = process.env.PORT || 8080;
@@ -121,8 +121,7 @@ app.get("/api/pendingviewer", function(req, res) {
     });
 });
 
-// TO DO: change to const uploader = async(path) or whatever
-async function uploader(file) {
+const uploader = async (file) => {
     return await cloudinary.uploader.upload(file, (error, result) => {
         if (error) console.log(error);
     });
@@ -135,19 +134,24 @@ const formatFile = file => {
     ).content;
 };
 
-// Mural viewer POST route
-// To do: change route to match /api/pending/:id format
-app.post("/api/pendingviewer", upload.array("image"), async function(req, res) {
+const uploadImages = async (files) => {
     const imageLinks = [];
-    for (file of req.files) {
+    for (file of files) {
         const formattedFile = formatFile(file);
         const result = await uploader(formattedFile);
         const url = result.url;
         imageLinks.push(url);
         // fs.unlinkSync(path);       
     }
-    
+    return imageLinks;
+}
+
+// Mural viewer POST route
+// To do: change route to match /api/pending/:id format
+app.post("/api/pendingviewer", upload.array("image"), async function(req, res) {
+    const imageLinks = await uploadImages(req.files);
     const formData = req.body;
+    
     const id = mongoose.Types.ObjectId();
     let mural = new PendingViewerMural({
         _id: id,
@@ -174,14 +178,9 @@ app.post("/api/pendingviewer", upload.array("image"), async function(req, res) {
 });
 
 // Mural artist POST route
-app.post("/api/pendingartist", upload.array("image"), function(req, res) {
+app.post("/api/pendingartist", upload.array("image"), async function(req, res) {
+    const imageLinks = await uploadImages(req.files);
     const formData = req.body;
-    
-    const images = req.files;
-    // To do: upload images to Google Drive
-
-    // for now: hardcoded links
-    const imageLinks = ['https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/reference_guide/outdoor_cat_risks_ref_guide/1800x1200_outdoor_cat_risks_ref_guide.jpg', 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg?h=c8d00152&itok=1fdekAh2'];
 
     const id = mongoose.Types.ObjectId();
     let mural = new PendingArtistMural({
